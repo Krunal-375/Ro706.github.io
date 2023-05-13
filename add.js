4e17165f6e61f5dd76da1fe56dbe675893cc9bf5
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2023-05-13 23:21:33 +0530

    Update add.js

--- add.js
+++ add.js
@@ -1,8 +1,8 @@
 const option1 = document.getElementById("option1"),
       option2 = document.getElementById("option2"),
       option3 = document.getElementById("option3"),
-      audio = document.getElementById("myAudio"); 
-      audio2 = document.getElementById("myAudio2") 
+      audio = document.getElementById("myAudio"),
+      audio2 = document.getElementById("myAudio2");
 var answer = 0;
 let score = 0;
 function generate_equation(){ 
@@ -32,6 +32,7 @@ function generate_equation(){
 option1.addEventListener("click", function(){
     if(option1.innerHTML == answer){
       correct.innerHTML = "Correct";
+      audio2.play();
       incorrect.innerHTML = "";
       score+=4;
       generate_equation();
@@ -47,6 +48,7 @@ option1.addEventListener("click", function(){
 option2.addEventListener("click", function(){
     if(option2.innerHTML == answer){
       correct.innerHTML = "Correct";
+      audio2.play();
       incorrect.innerHTML = "";
       score += 4;
       generate_equation();
@@ -62,6 +64,7 @@ option2.addEventListener("click", function(){
 option3.addEventListener("click", function(){
     if(option3.innerHTML == answer){
       correct.innerHTML = "Correct";
+      audio2.play();
       incorrect.innerHTML = "";
       score+=4;
       generate_equation();
